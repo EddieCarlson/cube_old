0c8356980e8cafc86ac03d741b60d7bd466a1956
Eddie Carlson <carlson.eddie@gmail.com>
2016-01-12 19:24:32 -0800

    reorder for arduino 1.6.7

--- cube_animations/cube_animations.ino
+++ cube_animations/cube_animations.ino
@@ -67,6 +67,42 @@ Cube cube(leds, xSize, ySize, zSize);
 int rainbow[180];
 int rainbow4[180];
 
+float colorFactor = 1.0;
+
+unsigned int h2rgb(unsigned int v1, unsigned int v2, unsigned int hue)
+{
+	if (hue < 60) return (v1 * 60 + (v2 - v1) * hue) * colorFactor;
+	if (hue < 180) return (v2 * 60) * colorFactor;
+	if (hue < 240) return (v1 * 60 + (v2 - v1) * (240 - hue)) * colorFactor;
+	return v1 * 60;
+}
+
+int makeColor(unsigned int hue, unsigned int saturation, unsigned int lightness)
+{
+	unsigned int red, green, blue;
+	unsigned int var1, var2;
+
+	if (hue > 359) hue = hue % 360;
+	if (saturation > 100) saturation = 100;
+	if (lightness > 100) lightness = 100;
+
+	// algorithm from: http://www.easyrgb.com/index.php?X=MATH&H=19#text19
+	if (saturation == 0) {
+		red = green = blue = lightness * 255 / 100;
+	} else {
+		if (lightness < 50) {
+			var2 = lightness * (100 + saturation);
+		} else {
+			var2 = ((lightness + saturation) * 100) - (saturation * lightness);
+		}
+		var1 = lightness * 200 - var2;
+		red = h2rgb(var1, var2, (hue < 240) ? hue + 120 : hue - 240) * 255 / 600000;
+		green = h2rgb(var1, var2, hue) * 255 / 600000;
+		blue = h2rgb(var1, var2, (hue >= 120) ? hue - 120 : hue + 240) * 255 / 600000;
+	}
+	return (red << 16) | (green << 8) | blue;
+}
+
 void setupRainbow() {
   int saturation = 100;
   int lightness = 50;
@@ -90,7 +126,7 @@ void solidCircle(Point *start, Point *color, float t) {
     return;
   }
   float radius = sqrt(t);
-  float near = min(2, radius / 1.5);
+  float near = std::min(2.0, radius / 1.5);
   float r2 = radius * radius;
   
   for(int x = 0; x < xSize; x++) {
@@ -874,38 +910,4 @@ void loop() {
 
 
 
-int makeColor(unsigned int hue, unsigned int saturation, unsigned int lightness)
-{
-	unsigned int red, green, blue;
-	unsigned int var1, var2;
 
-	if (hue > 359) hue = hue % 360;
-	if (saturation > 100) saturation = 100;
-	if (lightness > 100) lightness = 100;
-
-	// algorithm from: http://www.easyrgb.com/index.php?X=MATH&H=19#text19
-	if (saturation == 0) {
-		red = green = blue = lightness * 255 / 100;
-	} else {
-		if (lightness < 50) {
-			var2 = lightness * (100 + saturation);
-		} else {
-			var2 = ((lightness + saturation) * 100) - (saturation * lightness);
-		}
-		var1 = lightness * 200 - var2;
-		red = h2rgb(var1, var2, (hue < 240) ? hue + 120 : hue - 240) * 255 / 600000;
-		green = h2rgb(var1, var2, hue) * 255 / 600000;
-		blue = h2rgb(var1, var2, (hue >= 120) ? hue - 120 : hue + 240) * 255 / 600000;
-	}
-	return (red << 16) | (green << 8) | blue;
-}
-
-float colorFactor = 1.0;
-
-unsigned int h2rgb(unsigned int v1, unsigned int v2, unsigned int hue)
-{
-	if (hue < 60) return (v1 * 60 + (v2 - v1) * hue) * colorFactor;
-	if (hue < 180) return (v2 * 60) * colorFactor;
-	if (hue < 240) return (v1 * 60 + (v2 - v1) * (240 - hue)) * colorFactor;
-	return v1 * 60;
-}
